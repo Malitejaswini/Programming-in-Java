bdc9883e2b171fa2708732ae6c0985c3ba295ec2
Malitejaswini <99062996+Malitejaswini@users.noreply.github.com>
2022-03-17 18:48:08 +0530

    Add files via upload

---
+++ constructor.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class condemo
+{
+condemo(int x)
+{
+x=x*x;
+System.out.println(x);
+}
+}
+class constructor
+{
+public static void main(String args[])
+{
+condemo p1=new condemo(4);
+}
+}
