122ad51dbac7bb101ba0827110a0e7c0eef340b5
Malitejaswini <99062996+Malitejaswini@users.noreply.github.com>
2022-03-17 19:18:36 +0530

    Add files via upload

---
+++ cmdla.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class cmdla
+{
+	public static void main(String args[])
+	{
+		int a;
+		a=Integer.parseInt(args[0]);
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(a*i);
+		}
+	}
+}
