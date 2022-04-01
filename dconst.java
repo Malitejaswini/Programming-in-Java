067ba2f042c6986132bbdc25657dbc3257bdaba9
Malitejaswini <99062996+Malitejaswini@users.noreply.github.com>
2022-04-01 08:47:10 +0530

    Add files via upload

---
+++ dconst.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class econst
+{
+	econst()
+	{
+		System.out.println("example of default constructor");
+	}
+}
+class dconst
+{
+	public static void main(String args[])
+	{
+		econst p1=new econst();
+	}
+}
+
+
+
+
