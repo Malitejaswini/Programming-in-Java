8b416ea3ee76233dd6118dbffa673d26f41f634f
Malitejaswini <99062996+Malitejaswini@users.noreply.github.com>
2022-04-01 08:45:57 +0530

    Add files via upload

---
+++ pconst.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class pconst
+{
+	econst(int x,int y)
+	{
+		x=x*y;
+		System.out.println(x);
+	}
+}
+class pconst
+{
+	public static void main(String args[])
+	{
+		econst p1=new econst(4,5);
+	}
+}
