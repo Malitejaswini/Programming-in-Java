2c6587fedfe540a88295aff48d847d0d39690d6e
Malitejaswini <99062996+Malitejaswini@users.noreply.github.com>
2022-04-01 09:36:46 +0530

    Add files via upload

---
+++ forloop1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+ class forloop1
+ {
+   public static void main(String args[])
+   {
+    for (int i=4;i<=70;i=i*2)
+    {
+    System.out.println(i);
+    i=i+2;
+    System.out.println(i);
+   }
+   }
+ }
