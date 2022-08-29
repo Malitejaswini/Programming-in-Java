95834da4b855ffef6b6ba53e3874987a4cb1fecb
Tejaswini Mali <99062996+Malitejaswini@users.noreply.github.com>
2022-08-29 11:38:08 -0400

    Create create file in file handling.java

---
+++ create file in file handling.java
@@ -0,0 +1,24 @@
+import java.io.*;
+class fh
+{
+   public static void main(String args[])
+   {
+      File f=new File("hello.txt");
+      try
+      {
+
+		  if(f.createNewFile())
+		  {
+			  System.out.println("File Succesfully Created....");
+		  }
+		  else
+		  {
+			  System.out.println("File Already Exist...");
+		  }
+	  }
+	  catch(IOException i)
+	  {
+		  System.out.println("Exception Handeld....");
+      }
+   }
+}
